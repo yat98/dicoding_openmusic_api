0a9c1c78f953649a9c12f1969fff0aed08bd7922
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-01 20:21:02 +0800

    add: playlist activities migration

---
+++ migrations/1696161105704_create-playlist-activities-table.cjs
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    action: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('playlist_activities', 'fk_playlist_activities.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_activities', 'fk_playlist_activities.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_activities', 'fk_playlist_activities.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlist_activities', 'fk_playlist_activities.user_id_users.id');
+  pgm.dropConstraint('playlist_activities', 'fk_playlist_activities.playlist_id_playlists.id');
+  pgm.dropConstraint('playlist_activities', 'fk_playlist_activities.song_id_songs.id');
+  pgm.dropTable('playlist_activities');
+};
