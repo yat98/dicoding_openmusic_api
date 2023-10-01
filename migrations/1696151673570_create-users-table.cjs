15a2151e8bd75ae6121b387042ad2333fd78fca4
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-01 17:23:11 +0800

    add: users migration

---
+++ migrations/1696151673570_create-users-table.cjs
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    username: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    token: {
+      type: 'TEXT',
+      notNull: false,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
