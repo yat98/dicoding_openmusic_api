fef2deded3a4d719c878de288c4988139b73e95b
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-01 18:27:29 +0800

    add: constraint user_id in playlists migration

---
+++ migrations/1696155776351_create-playlists-table.cjs
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+  pgm.addConstraint('playlists', 'fk_playlists.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlists', 'fk_playlists.user_id_users.id');
+};
